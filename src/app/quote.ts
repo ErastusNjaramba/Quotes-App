0af3e3a58737742c546dcd2a71115d57ff542937
ErastusNjaramba <erastus.kimani@student.moringaschool.com>
2021-07-18 04:58:17 +0300

    update date array

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,7 @@
 export class Quote {
   constructor(
   public id: number,
+  public name: string,
   public words: string,
   public author: string,
   public datePosted: Date
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,12 +10,12 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[] = [
-   new Quote (2, 'When you reach the end of your rope, tie a knot in it and hang on.', 'Franklin D. Roosevelt',new Date(2021,7,18)),
-   new Quote (3, 'The future belongs to those who believe in the beauty of their dreams.', 'Eleanor Roosevelt',new Date(2021,7,18)),
-   new Quote (1, 'Life is what happens when you`re busy making other plans.', 'John Lennon',new Date(2021,7,18)),
-   new Quote (4, 'Whoever is happy will make others happy too.', 'Anne Frank',new Date(2021,7,18)),
-   new Quote (5, 'Tell me and I forget. Teach me and I remember. Involve me and I learn.', 'Benjamin Franklin',new Date(2021,7,18)),
-   new Quote (6, 'In the end, it`s not the years in your life that count. It`s the life in your years. ', 'Abraham Lincoln',new Date(2021,7,18)),
+   new Quote (2, 'Eratus', 'When you reach the end of your rope, tie a knot in it and hang on.', 'Franklin D. Roosevelt',new Date(2021,3,18)),
+   new Quote (3, 'Antony', 'The future belongs to those who believe in the beauty of their dreams.', 'Eleanor Roosevelt',new Date(2021,4,16)),
+   new Quote (1, 'Rayan', 'Life is what happens when you`re busy making other plans.', 'John Lennon',new Date(2021,5,12)),
+   new Quote (4, 'Mike', 'Whoever is happy will make others happy too.', 'Anne Frank',new Date(2021,6,12)),
+   new Quote (5, 'Tyrel', 'Tell me and I forget. Teach me and I remember. Involve me and I learn.', 'Benjamin Franklin',new Date(2021,6,13)),
+   new Quote (6, 'Tysha', 'In the end, it`s not the years in your life that count. It`s the life in your years. ', 'Abraham Lincoln',new Date(2021,6,17)),
   ];
   
 
